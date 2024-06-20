d0dcb74e30234ca2787fd5ed825d55b701e4b004
weinsi <azhi83s@outlook.com>
2024-06-20 23:58:02 +0800

    Modify check.cpp

--- check.cpp
+++ check.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Size whole fish option author.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
