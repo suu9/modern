597499e4b36009f8ec5103df4d510f4e2a8953cf
weinsi <azhi83s@outlook.com>
2024-06-27 23:01:33 +0800

    Modify manage.go

--- manage.go
+++ manage.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# As maybe environmental mission see article.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
