7caebfbccce566378130a3c574d3ef611bb08a3e
weinsi <azhi83s@outlook.com>
2024-07-15 12:49:37 +0800

    Modify minute.cpp

--- minute.cpp
+++ minute.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Most share challenge.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
