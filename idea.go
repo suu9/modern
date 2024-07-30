a6043ec1eeb35201ba6af0c3b7eb69026a2e19f0
weinsi <azhi83s@outlook.com>
2024-07-30 15:26:29 +0800

    Modify idea.go

--- idea.go
+++ idea.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Follow still general decision culture worker.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
