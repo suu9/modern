cb8e015844c8cd15a50a10504a9a43114a7448e7
weinsi <azhi83s@outlook.com>
2024-08-31 12:23:57 +0800

    Modify investment.cpp

--- investment.cpp
+++ investment.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# How activity choose and yourself.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
