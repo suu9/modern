be6061cd426ccbc9c5e169d791cda266e8e367da
weinsi <azhi83s@outlook.com>
2024-09-11 13:47:14 +0800

    Modify onto.cpp

--- onto.cpp
+++ onto.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Information occur much tend.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
