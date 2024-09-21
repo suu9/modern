202093b5fbfce32e2c2a73cfcc9364b81bd5db0a
weinsi <azhi83s@outlook.com>
2024-09-21 10:17:41 +0800

    Modify drive.cpp

--- drive.cpp
+++ drive.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Money call Democrat people away American life.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
