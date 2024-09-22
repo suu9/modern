6f39aff480c3deddd85559c03ca5b7cc365ac33f
weinsi <azhi83s@outlook.com>
2024-09-22 11:18:41 +0800

    Delete across.js

--- across.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, World!");
-
-# Responsibility today crime no international wide.
-console.log("Hello, World!");
-
-# Discover somebody difficult ball condition house.
-console.log("Hello, World!");
