546277bdf8af70fb117c0636dbf512c47e838b1d
weinsi <azhi83s@outlook.com>
2024-10-30 15:50:45 +0800

    Modify participant.cpp

--- participant.cpp
+++ participant.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Occur rise difference small.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
