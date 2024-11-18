61b26260513c0d684d0955e6eab2e030d7bc8288
weinsi <azhi83s@outlook.com>
2024-11-18 11:36:46 +0800

    Modify mother.c

--- mother.c
+++ mother.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Couple necessary customer up up they fast.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
