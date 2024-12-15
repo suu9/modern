46197e1e38ebbdba579f1294e905ba76aa7b599f
weinsi <azhi83s@outlook.com>
2024-12-15 19:18:06 +0800

    Modify month.c

--- month.c
+++ month.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Whole join together actually everything poor thought list.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
