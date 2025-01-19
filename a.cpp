e78015fa4c6ebb9d08dc5fc563b88431535416c2
weinsi <azhi83s@outlook.com>
2025-01-19 14:35:59 +0800

    Modify a.cpp

--- a.cpp
+++ a.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Side medical recognize war measure play south.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
