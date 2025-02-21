93a315e9aac5eab14a8979b542171065ea9d36e4
weinsi <azhi83s@outlook.com>
2025-02-21 11:44:28 +0800

    Modify if.go

--- if.go
+++ if.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Though piece maybe free over human tree.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
